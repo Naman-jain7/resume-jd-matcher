e2bbf980559d3b7cd00a78a18cf8dd20c2afd314
Naman Jain <166692314+Naman-jain7@users.noreply.github.com>
2026-01-18 23:13:46 +0530

    Delete chatbot directory

--- chatbot/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse,HttpResponseNotFound
-from django.core.cache import cache
-from datetime import timedelta
-from django.contrib import messages
-
-def chatbot(request):
-    pass
