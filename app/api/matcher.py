4069a543aca53138baf612db748a4cf437bf5d84
Naman-jain7 <nj323875@gmail.com>
2026-01-20 02:24:20 +0530

    save response model

--- app/api/matcher.py
+++ app/api/matcher.py
@@ -1,10 +1,37 @@
+# http routes, validate request inputs, handle api level errors
 from fastapi import FastAPI, APIRouter, UploadFile, File, Form, HTTPException
 from fastapi.responses import JSONResponse
 from typing import Optional, List, Dict, Any
+from app.models.schemas import MatchResponse
 import uuid
 
 matcher_router = APIRouter(prefix='/api', tags=['matcher'])
 
+SUPPORTED_FILE_TYPES = {
+    "application/pdf",
+    "application/msword",
+    "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    "text/plain",
+}
+
 @matcher_router.post('/match')
-async def match_resume(resume: UploadFile = File(...), jd: str = Form(...)):
-    """ Match resume to job description using AI """
+async def match_resume(resume_file: UploadFile = File(...), job_description: str = Form(...))->MatchResponse:
+    """ Match resume to job description using AI. Business logic layer will be delegated to langgraph later"""
+    if resume_file.content_type not in SUPPORTED_FILE_TYPES:
+        raise HTTPException(status_code=400, detail="Unsupported file type")
+
+    if not job_description.strip():
+        raise HTTPException(status_code=400, detail="JD can't be empty")
+
+    request_id = str(uuid.uuid4())
+
+    # ------------STUB RESPONSE---------------------
+    return MatchResponse(
+        request_id=request_id,
+        match_score=0,
+        matched_skills=[],
+        missing_skills=[],
+        rewrite_suggestions=[],
+        status="stub_response",
+    )
+    # ------------STUB RESPONSE---------------------
---
+++ app/models/schemas.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel, Field
+from typing import List, Annotated, Optional
+
+class MatchResponse(BaseModel):
+    request_id: Annotated[str, Field(..., description='Request ID for match request')]
+    match_score: Annotated[int, Field(..., ge=0, le=100, description='Match score between 0 and 100')]
+    matched_skills: Annotated[Optional[List[str]], Field(default_factory=list, description='Skills found in both resume and job description')]
+    missing_skills: Annotated[Optional[List[str]], Field(default_factory=list, description='Skills required in job description but missing in resume')]
+    rewrite_suggestions: Annotated[List[str], Field(default_factory=list, description="Resume rewrite suggestions aligned to JD")]
+    status: Annotated[str, Field(..., description='Processing status of the request')]
